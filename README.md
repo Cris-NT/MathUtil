bca9c3f45668e467cc7ed25ce8dc9fc22a5d9b88
Cris-NT <nhattienbr456@gmail.com>
2020-07-10 08:24:50 +0700

    1st: the first upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# Welcome to CrisNT repository
+This repo contains the source code of a simple Java application used to 
+demonstrate the CI - Continuous Integration process - a part of 
+Introfuction to Sogtware Engineering 
+course at FPT University - HCMC Campus
+
+## You will find in this repo:
+* MathUtil class - holds the list of math methods
+* Test class - demonstrate on Unit Testing using TDD/JUnit
+* .gitignore
+* Markdown file - this file itself
+
+## Connect me via
+1. [Github](https://github.com/Cris-NT)
+2. [Facebook](https://www.facebook.com/cris.2k.crssoft)
+
+#### © 2020 by CrisNT
---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+/**
+ *
+ * @author ADMIN
+ */
+// day la class chua cac ham toan hoc tinh toan, voi muc tieu xai chung cho cac noi khac, pham cai gi mang tinh chat xai chung, ta se dung STATIC
+public class MathUtil {
+
+    //tinh n! = 1.2.3...n. Vi n! tang gia tri rat nhanh, som tran mien int
+    //int chi toi da 2ty1, nen ta xai long moi an toan
+    //thuc ra 15! tolam roi, nen ta chi tinh giai thua tu 15 tro lai
+    public static long computeFactory(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+}
