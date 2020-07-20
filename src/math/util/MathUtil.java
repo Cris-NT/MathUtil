206f3155c75f5c8404f4d32e160d6d50a7415beb
Cris-NT <nhattienbr456@gmail.com>
2020-07-20 07:32:57 +0700

    3rd: add the exception handler into cF()

--- src/math/util/MathUtil.java
+++ src/math/util/MathUtil.java
@@ -16,6 +16,9 @@ public class MathUtil {
     //int chi toi da 2ty1, nen ta xai long moi an toan
     //thuc ra 15! tolam roi, nen ta chi tinh giai thua tu 15 tro lai
     public static long computeFactory(int n) {
+        if(n<0){
+            throw new IllegalArgumentException("Invalid Input. n must be >=0");
+        }
         long result = 1;
         for (int i = 1; i <= n; i++) {
             result *= i;
